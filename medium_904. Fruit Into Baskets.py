84f5c25fd34a37afda6ffa94b90ac5830fcf8ae6
sdksfo <sdksfo@gmail.com>
2019-01-19 13:36:01 -0800

    Create medium_904. Fruit Into Baskets.py

---
+++ medium_904. Fruit Into Baskets.py
@@ -0,0 +1,44 @@
+# Explanation:
+
+# We maintain a running basket of two fruits.
+
+# We maintain a `tuple` of counts for each tree in the DP table:
+
+# idx[0] = increment if the fruit can be added to the basket
+# idx[1] = increment if the left tree has the same fruit as current tree
+
+# If the fruit is already in the basket:
+# 	dp[i][0] = dp[i-1][0] + 1
+#   dp[i][1] = dp[i-1][1] + 1 (if left fruit is same as current fruit) or 1
+
+# if the fruit is not in the basket:
+#   basket = fruit and left tree's fruit
+#   dp[i][0] = dp[t-1][1] + 1 (we increment based on the left tree's fruit)
+#   dp[i][1] = 1
+
+# (e.g)
+# tree   = [ 1,     1,     2,     2,     1,     2,     2,     3,      3,     3,     2,     3,     2,     4,    3,        3 ]
+# dp     = [(1,1),(2,2), (3,1), (4,2), (5,1), (6,1), (7,2), (3, 1), (4,2), (5,3), (6,1), (7,1), (8,1),  (2,1), (2,1), (3,2)]
+# basket = |             (1,2)                         |                   (3,2)                  |(2,4) |    (4,3)      |
+
+# max fruits = 8
+
+class Solution(object):
+    def totalFruit(self, tree):
+        """
+        :type tree: List[int]
+        :rtype: int
+        """
+        dp, basket = [(1,1)] * len(tree), set((tree[0],))
+        max_fruits = 1
+
+        for t in xrange(1, len(tree)):
+        	if tree[t] in basket:
+        		dp[t] = (dp[t-1][0] + 1, dp[t-1][1] + 1 if tree[t] == tree[t-1] else 1)
+        	else:
+        		basket = set((tree[t], tree[t-1]))
+        		dp[t] = (dp[t-1][1] + 1, 1)
+        	max_fruits = max(max_fruits, dp[t][0])
+        return max_fruits
+
+print Solution().totalFruit([3,3,3,1,2,1,1,2,3,3,4])
