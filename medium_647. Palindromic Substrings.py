a1a242684db3ae8207cd281eb7a78f2d5f8c991e
sdksfo <sdksfo@gmail.com>
2019-01-21 22:09:31 -0800

    Update medium_647. Palindromic Substrings.py
    Remove sum operation

--- medium_647. Palindromic Substrings.py
+++ medium_647. Palindromic Substrings.py
@@ -9,18 +9,18 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        dp = [[0 for i in xrange(len(s))] for i in xrange(len(s))]
+        dp, cnt = [[0 for i in xrange(len(s))] for i in xrange(len(s))], len(s)
 
         for i in xrange(len(s)): dp[i][i] = 1
 
         for row in xrange(len(s)-2, -1, -1):
-        	for col in xrange(row+1, len(s)):
-        		if s[row] == s[col]:
-        			if row + 1 == col: # substring of length 2
-        				dp[row][col] = 1
-        			else:
-        				dp[row][col] = 1 if dp[row+1][col-1] > 0 else 0
-
-        return sum([sum(i) for i in dp])
+            for col in xrange(row+1, len(s)):
+                if s[row] == s[col]:
+                    if row + 1 == col: # substring of length 2
+                        dp[row][col] = 1
+                    else:
+                        dp[row][col] = 1 if dp[row+1][col-1] else 0
+                    cnt += dp[row][col]
+        return cnt
 
 print Solution().countSubstrings('abbded')
