533138896111995ab0426aa079d373844bfb2a94
sdksfo <sdksfo@gmail.com>
2019-01-21 17:42:33 -0800

    Create medium_49. Group Anagrams.py

---
+++ medium_49. Group Anagrams.py
@@ -0,0 +1,20 @@
+
+# Explanation
+
+# anagrams when sorted yields the same string e.g tea and ate when sorted gives 'aet'. Group each
+# anagram using its sorted string as key.
+
+import collections
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        hashmap = collections.defaultdict(list)
+        for str_ in strs:
+        	hashmap[''.join(sorted(str_))].append(str_)
+        return [hashmap[k] for k in hashmap]
+
+print Solution().groupAnagrams([])
