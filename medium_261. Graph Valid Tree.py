0cc73118544c6039f6a4ef58e4a71b596d2743db
sdksfo <sdksfo@gmail.com>
2019-02-20 17:18:26 -0800

    Update medium_261. Graph Valid Tree.py

--- medium_261. Graph Valid Tree.py
+++ medium_261. Graph Valid Tree.py
@@ -4,30 +4,18 @@ The idea is the same as finding if there is a cycle using UnionFind
 """
 
 class Solution(object):
-
-    def getParent(self, vertex):
-        if self.parent[vertex] == -1:
-            return vertex
-        return self.getParent(self.parent[vertex])
-
-    def union(self, u, v):
-        self.parent[u] = v
-
+    def getParent(self, v):
+        if v in self.parent:
+            return self.getParent(self.parent[v])
+        return v
     def validTree(self, n, edges):
-        """
-        :type n: int
-        :type edges: List[List[int]]
-        :rtype: bool
-        """
-        self.parent = {i:-1 for i in xrange(n)}
-
-        for u,v in edges:
-            parent_u = self.getParent(u)
-            parent_v = self.getParent(v)
-            if parent_u == parent_v:
+        self.parent = {}
+        for u, v in edges:
+            parent_1 = self.getParent(u)
+            parent_2 = self.getParent(v)
+            if parent_1 == parent_2:
                 return False
-            self.union(parent_u, parent_v)
-
+            self.parent[parent_1] = parent_2
         return len(edges) == n-1 # This step determines if all components are connected or not
 
 print Solution().validTree(4, [[0,1],[2,3]])
