3a2a4bfe03f9af039e1c4284a814b430ce01a0f7
sdksfo <sdksfo@gmail.com>
2019-02-23 00:29:16 -0800

    Update medium_413. Arithmetic Slices.py

--- medium_413. Arithmetic Slices.py
+++ medium_413. Arithmetic Slices.py
@@ -1,10 +1,14 @@
-
 """
-Every digit that gets added to the sequence adds 'n' more possibilities to the output where 'n' is the difference between number of digits in array and 3.
+Every digit that gets added to the sequence adds 'n' more possibilities to the output where 'n' is the offset from 3.
 
-e.g For [1,2,3] we have [1,2,3]
-For [1,2,3,4] we have [1,2,3], [1,2,3,4], [2,3,4] ie it adds 2 new sub-arrays ending with 4.
-For [1,2,3,4,5], we have [1,2,3,4,5], [2,3,4,5], [3,4,5] ie it adds 3 new sub-arrays ending with 5 to whatever we could make with 4 elements ie [1,2,3,4]
+e.g For 3 elements, we have 1 sub-array [1,2,3] ending with 3
+For 4 elements [1,2,3,4] we have [1,2,3,4], [2,3,4] ie it adds 2 new sub-arrays ending with 4.
+For 5 elements [1,2,3,4,5], we have [1,2,3,4,5], [2,3,4,5], [3,4,5] ie it adds 3 new sub-arrays ending with 5.
+Likewise for 6 elements, we will have 4 new sub-arrays that'd be added, so on and forth.
+
+TLDR:
+
+So the logic is to add 1 for every single element that gets added in the sequence from size of 3.
 
 """
 
