2dda4a3d430352496d7e22c7d12e598dacb100b9
sdksfo <sdksfo@gmail.com>
2019-02-24 20:50:06 -0800

    Update medium_131. Palindrome Partitioning.py

--- medium_131. Palindrome Partitioning.py
+++ medium_131. Palindrome Partitioning.py
@@ -7,21 +7,18 @@
 # ]
 
 class Solution(object):
+    hashmap = {'':[[]]}
 
     def isPalin(self, substring):
         return substring == substring[::-1]
 
     def partition(self, s):
-        if not s: return [[]]
+        if s in self.hashmap: return self.hashmap[s]
         output = []
-        others = self.partition(s[1:])
-        for other in others:
-            curr = s[0]
-            output.append([curr] + other)
-            for p in xrange(len(other)):
-                curr += other[p]
-                if curr[0] == curr[-1] and self.isPalin(curr) and [curr] + other[p+1:] not in output:
-                    output.append([curr] + other[p+1:])
+        for i in range(1, len(s)+1):
+            if self.isPalin(s[:i]):
+                output.extend([s[:i]] + j for j in self.partition(s[i:]))
+        self.hashmap[s] = output
         return output
 
 print Solution().partition('aaabab')
