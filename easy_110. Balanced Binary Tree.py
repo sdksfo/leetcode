3936156876299969e72cd0186f2a978f90a45af2
sdksfo <sdksfo@gmail.com>
2019-03-14 22:38:24 -0700

    Update easy_110. Balanced Binary Tree.py

--- easy_110. Balanced Binary Tree.py
+++ easy_110. Balanced Binary Tree.py
@@ -1,1 +1,25 @@
-nodes = [1,2,2,3,3,3,3,4,4,4,4,4,4,None,None,5,5]
+class Solution(object):
+    def isBalanced(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        hashmap, stack = {}, []
+
+        def height(root):
+            if root in hashmap: return hashmap[root]
+            if root:
+                hashmap[root] = 1 + max(height(root.left), height(root.right))
+            return hashmap.get(root, 0)
+
+        while stack or root:
+            if root:
+                stack.append(root)
+                root = root.left
+            else:
+                root = stack.pop()
+                if abs(height(root.left) - height(root.right)) > 1:
+                    return False
+                root = root.right
+
+        return True
