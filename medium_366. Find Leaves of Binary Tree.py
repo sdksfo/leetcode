057d6bdcfa651de84389ce69d006c295242d3b80
sdksfo <sdksfo@gmail.com>
2019-03-14 19:27:32 -0700

    Create medium_366. Find Leaves of Binary Tree.py

---
+++ medium_366. Find Leaves of Binary Tree.py
@@ -0,0 +1,36 @@
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def removeLeaf(self, root, leaves):
+        if root:
+            if root.left and not root.left.left and not root.left.right:
+                leaves.append(root.left.val)
+                root.left = None
+            if root.right and not root.right.left and not root.right.right:
+                leaves.append(root.right.val)
+                root.right = None
+            self.removeLeaf(root.left, leaves)
+            self.removeLeaf(root.right, leaves)
+        return leaves
+
+    def findLeaves(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+
+        output = []
+
+        while root and (root.left or root.right):
+            output.append(self.removeLeaf(root, []))
+
+        output.append([root.val]) if root else []
+
+        return output
+
