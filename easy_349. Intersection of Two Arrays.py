476c773d0e3636d86a363636f3f0c2d7b870063e
sdksfo <sdksfo@gmail.com>
2019-04-18 12:28:01 -0700

    Update easy_349. Intersection of Two Arrays.py

--- easy_349. Intersection of Two Arrays.py
+++ easy_349. Intersection of Two Arrays.py
@@ -1,14 +1,32 @@
+"""
+Approach:
 
+a) Use shorter array and binary search on the longer array
+
+Time: O(logn) Space: O(1)
+"""
 
 class Solution(object):
+    def find_num(self, n, target, i, j):
+        if i > j:
+            return -1
+        mid = (i+j)/2
+        if n[mid] == target:
+            return mid
+        elif n[mid] > target:
+            return self.find_num(n, target, i, mid-1)
+        else:
+            return self.find_num(n, target, mid+1, j)
+
+    def search(self, n1, n2):
+        return [i for i in set(n1) if self.find_num(n2, i, 0, len(n2)-1) != -1]
+
     def intersection(self, nums1, nums2):
         """
         :type nums1: List[int]
         :type nums2: List[int]
         :rtype: List[int]
         """
-        nums1, nums2 = set(nums1), set(nums2)
-        return [i for i in nums1 if i in nums2]
+        nums1, nums2 = sorted(nums1), sorted(nums2)
 
-
-print Solution().intersection([1,2,3,7,8,9,2], [4,5,2,3,2])
+        return self.search(nums1, nums2) if len(nums1) < len(nums2) else self.search(nums2, nums1)
