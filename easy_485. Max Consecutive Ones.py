bd19d0cc88e93a9113c11d4d1eef0745dd098d13
sdksfo <sdksfo@gmail.com>
2019-04-17 23:43:50 -0700

    Update easy_485. Max Consecutive Ones.py

--- easy_485. Max Consecutive Ones.py
+++ easy_485. Max Consecutive Ones.py
@@ -1,20 +1,19 @@
 
 
+
 class Solution(object):
     def findMaxConsecutiveOnes(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-        max_count, prev, ctr = 0, 0, 0
+        ctr, max_ctr = 0, 0
 
         for num in nums:
-        	if num == 1:
-        		ctr += 1
-        	if not num and prev:
-        		max_count = max(ctr, max_count)
+        	if num == 0:
+        		max_ctr = max(max_ctr, ctr)
         		ctr = 0
-        	prev = num
-        return max(max_count, ctr)
+        	else:
+        		ctr += 1
 
-print Solution().findMaxConsecutiveOnes([0])
+        return max(max_ctr, ctr)
