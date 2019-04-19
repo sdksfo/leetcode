a11b63c3e1b924e0604a24ed870a1f4f4379762c
sdksfo <sdksfo@gmail.com>
2019-04-18 17:42:13 -0700

    Update medium_50. Pow(x, n).py

--- medium_50. Pow(x, n).py
+++ medium_50. Pow(x, n).py
@@ -7,17 +7,15 @@ class Solution(object):
         :type n: int
         :rtype: float
         """
-        def getPow(x, n):
-            if n == 0:
-                return float(1)
+        def compute(x, n):
+        	if n == 0:
+        		return float(1)
 
-            half_num = getPow(x, n/2)
+        	partial = compute(x, float(n)/2)
 
-            if n % 2:
-                return half_num * half_num * x
-            else:
-                return half_num * half_num
+        	if n%2:
+        		return x * partial * partial
+        	else:
+        		return partial * partial
 
-        return getPow(x, n) if n > 0 else 1/getPow(x, abs(n))
-
-print Solution().myPow(2.00000, 10)
+        return 1/compute(x, abs(n)) if n < 0 else compute(x, n)
