29cdfbc574261afd6c9af6dd4ac301742665f8ca
sdksfo <sdksfo@gmail.com>
2019-04-20 18:07:04 -0700

    Update medium_153. Find Minimum in Rotated Sorted Array.py

--- medium_153. Find Minimum in Rotated Sorted Array.py
+++ medium_153. Find Minimum in Rotated Sorted Array.py
@@ -1,24 +1,14 @@
-"""
-
-Things to note here:
-
-a) why is 'j' assigned mid instead of mid - 1  ===> since nums[mid] < nums[j], mid could be the smallest in the array technically. So we include
-mid in our next iteration
-b) Why is return nums[j] instead of nums[i] ===> Typically in binary search, i moves from left to right and j from right to left.
-But here, only i is moving and j is not moving. So if we start 'i' at 0 and 'j' at 5, 'i' ptr is the only one moving and hence
-it would move beyond 'j' for the loop to terminate. Hence we return 'j' instead of 'i'.
-
-"""
 
 class Solution(object):
     def findMin(self, nums):
-    	i, j = 0, len(nums)-1
+        i, j = 0, len(nums)-1
 
-        while i <= j:
+        while i < j:
             mid = (i+j)/2
+            if nums[mid] < nums[mid-1]:
+                return nums[mid]
             if nums[mid] < nums[j]:
                 j = mid
             else:
                 i = mid + 1
-
-        return nums[j]
+        return nums[i]
