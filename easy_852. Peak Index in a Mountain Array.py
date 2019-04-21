30a4d8aaf1b01aa14d352290f1f3570f050c40af
sdksfo <sdksfo@gmail.com>
2019-04-20 18:07:17 -0700

    Update easy_852. Peak Index in a Mountain Array.py

--- easy_852. Peak Index in a Mountain Array.py
+++ easy_852. Peak Index in a Mountain Array.py
@@ -9,12 +9,12 @@ class Solution(object):
         i, j = 0, len(A)
 
         while i < j:
-            mid = i + (j-i)/2
+            mid = (i+j)/2
+            if A[mid-1] < A[mid] > A[mid+1]:
+                return mid
             if A[mid] < A[mid+1]:
                 i = mid + 1
             else:
                 j = mid
-
-        return i
 
 print Solution().peakIndexInMountainArray([0,1,2,3,4,5,6,7,8,9,3,2,2,1,1,0])
