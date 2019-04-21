e5f7feafb0823181402e1022b0a24117d12ec2ea
sdksfo <sdksfo@gmail.com>
2019-04-20 19:11:15 -0700

    Update easy_167. Two Sum II - Input array is sorted.py

--- easy_167. Two Sum II - Input array is sorted.py
+++ easy_167. Two Sum II - Input array is sorted.py
@@ -1,22 +1,15 @@
 
-"""Since array is sorted, we can use the two pointer technique"""
 
 class Solution(object):
     def twoSum(self, numbers, target):
-        """
-        :type numbers: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
         i, j = 0, len(numbers)-1
 
         while i < j:
-        	total = numbers[i] + numbers[j]
-        	if  total == target:
-        		return [i+1, j+1]
-        	elif total < target:
-        		i += 1
-        	else:
-        		j -= 1
+            if numbers[i] + numbers[j] == target:
+                return i+1, j+1
+            elif numbers[i] + numbers[j] > target:
+                j -= 1
+            else:
+                i += 1
 
-print Solution().twoSum([2,7,11,15], 22)
+print Solution().twoSum([5,25,75], 100)
