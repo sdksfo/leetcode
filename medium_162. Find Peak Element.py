d546b02047d99338c06856e0eade9d0fc77f7e08
sdksfo <sdksfo@gmail.com>
2019-04-21 22:04:21 -0700

    Update medium_162. Find Peak Element.py

--- medium_162. Find Peak Element.py
+++ medium_162. Find Peak Element.py
@@ -1,5 +1,6 @@
 
-# O(n) solution is easy as we need to just traverse the array. Lets implement that first and then probably do the binary search version
+
+
 
 class Solution(object):
     def findPeakElement(self, nums):
@@ -7,26 +8,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        if len(nums) == 1: return 0
-        for i in xrange(len(nums)):
-          if i > 0 and i < len(nums)-1:
-              if nums[i-1] < nums[i] > nums[i+1]: return i
-          elif i == 0:
-              if nums[i] > nums[i+1]: return i
-          elif i == len(nums) - 1:
-              if nums[i] > nums[i-1]: return i
+        i, j = 0, len(nums)
 
-print Solution().findPeakElement([1,2,1,3,5,6,4])
-
-class Solution(object):
-    def findPeakElement(self, nums):
-        i , j = 0 , len(nums) - 1
         while i < j:
-            mid = (i + j)/2
-            if nums[mid] > nums[mid+1]:
-                j = mid
-            else:
-                i = mid+1
+        	mid = (i + j)/2
+
+        	if nums[mid-1] < nums[mid] > nums[(mid+1)%len(nums)]:
+        		return mid
+        	elif nums[mid-1] < nums[mid] < nums[(mid+1)%len(nums)]:
+        		i = mid + 1
+        	else:
+        		j = mid
+
         return i
 
-print Solution().findPeakElement([1,2,1,1,1,1])
+print Solution().findPeakElement([0,1,2,3,4,2])
