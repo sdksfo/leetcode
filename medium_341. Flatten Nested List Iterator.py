5b4ecee8f34422b4681b633782c6ceee59fbb083
sdksfo <sdksfo@gmail.com>
2019-05-01 23:22:49 -0700

    Create medium_341. Flatten Nested List Iterator.py

---
+++ medium_341. Flatten Nested List Iterator.py
@@ -0,0 +1,31 @@
+
+"""
+Create a new stack and store the nestedinteger. If the current top element in the stack is int, returns it
+else, pop the element, get its list and add back to the stack
+"""
+
+class NestedIterator(object):
+
+    def __init__(self, nestedList):
+        """
+        Initialize your data structure here.
+        :type nestedList: List[NestedInteger]
+        """
+        self.stack = nestedList[::-1]
+
+    def next(self):
+        """
+        :rtype: int
+        """
+        return self.stack.pop().getInteger()
+
+    def hasNext(self):
+        """
+        :rtype: bool
+        """
+        while self.stack:
+            top = self.stack[-1]
+            if top.isInteger():
+                return True
+            self.stack = self.stack[:-1] + top.getList()[::-1]
+        return False
