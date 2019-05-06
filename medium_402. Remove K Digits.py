40efdbe77e98f9893962b8f40b9a599141e6da9b
sdksfo <sdksfo@gmail.com>
2019-05-06 15:08:43 -0700

    Update medium_402. Remove K Digits.py

--- medium_402. Remove K Digits.py
+++ medium_402. Remove K Digits.py
@@ -4,6 +4,8 @@
 # Iterating from left, for each digit check if the next digit is smaller than it. If so, remove the current digit. e.g 1419 remove '4'
 # If none of the digits are removed at a step, remove the last digit e.g 112, remove '2'
 
+# Use a stack data structure to repeatedly remove bigger numbers.
+
 class Solution(object):
     def removeKdigits(self, num, k):
         """
@@ -11,13 +13,14 @@ class Solution(object):
         :type k: int
         :rtype: str
         """
-        for i in xrange(k):
-            for j in xrange(len(num)-1):
-                if num[j] > num[j+1]:
-                    num = num[:j]+num[j+1:]
-                    break
-            else:
-                num = num[:-1]
-        return str(int(num)) if num else '0'
+        stack = []
 
-print Solution().removeKdigits('1433219', 2)
+        for digit in num:
+            while k and stack and stack[-1] > digit:
+                stack.pop()
+                k -= 1
+            stack.append(digit)
+
+        return str(int((''.join((stack[:-k] if k else stack) or ['0']))))
+
+print Solution().removeKdigits('1034', 3)
