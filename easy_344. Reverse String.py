dfccaba6ef1475a9a549bb795410e17d6737c74d
sdksfo <sdksfo@gmail.com>
2019-05-07 23:39:38 -0700

    Update easy_344. Reverse String.py

--- easy_344. Reverse String.py
+++ easy_344. Reverse String.py
@@ -1,15 +1,16 @@
 
+
 class Solution(object):
     def reverseString(self, s):
         """
         :type s: List[str]
-        :rtype: void Do not return anything, modify s in-place instead.
+        :rtype: None Do not return anything, modify s in-place instead.
         """
-        i, j = 0, len(s) - 1
+        i, j = 0, len(s)-1
 
         while i < j:
         	s[i], s[j] = s[j], s[i]
         	i += 1
         	j -= 1
 
-print Solution().reverseString([])
+print Solution().reverseString(["h","e","l","l","o"])
