1a29794bf744def0751c8985dbb4eaae986a3223
sdksfo <sdksfo@gmail.com>
2019-05-08 12:12:51 -0700

    Update easy_345. Reverse Vowels of a String.py

--- easy_345. Reverse Vowels of a String.py
+++ easy_345. Reverse Vowels of a String.py
@@ -1,5 +1,4 @@
 
-"""Use two pointers and swap only if vowels are encountered"""
 
 class Solution(object):
     def reverseVowels(self, s):
@@ -7,18 +6,18 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        i, j, vowels, s = 0, len(s)-1, set('aeiouAEIOU'), list(s)
+        s, i, j, vowels = list(s), 0, len(s)-1, set('AEIOUaeiou')
 
         while i < j:
-        	if s[i] in vowels and s[j] in vowels:
-        		s[i], s[j] = s[j], s[i]
-        		i += 1
-        		j -= 1
-        	elif s[i] not in vowels:
+        	if s[i] not in vowels:
         		i += 1
         	elif s[j] not in vowels:
+        		j -= 1
+        	elif s[i] in vowels and s[j] in vowels:
+        		s[i], s[j] = s[j], s[i]
+        		i += 1
         		j -= 1
 
         return ''.join(s)
 
-print Solution().reverseVowels('Aa')
+print Solution().reverseVowels('leetcode')
