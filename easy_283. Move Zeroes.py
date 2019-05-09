89e45c896e6f51287be270bf9bf01a384ed477d9
sdksfo <sdksfo@gmail.com>
2019-05-08 19:59:53 -0700

    Update easy_283. Move Zeroes.py

--- easy_283. Move Zeroes.py
+++ easy_283. Move Zeroes.py
@@ -1,26 +1,18 @@
 
-"""
-Iterate through the array, if a zero is encountered, mark the position as
-the first 0th index. If a number is found, swap it with the 0th index and
-increase the 0th index value.
-"""
 
 class Solution(object):
     def moveZeroes(self, nums):
         """
         :type nums: List[int]
-        :rtype: void Do not return anything, modify nums in-place instead.
+        :rtype: None Do not return anything, modify nums in-place instead.
         """
-        zero_index, i = None, 0
+        zero_idx = 0
 
-        while i < len(nums):
-        	if nums[i] == 0 and zero_index is None:
-        		zero_index = i
-        	if nums[i] != 0 and zero_index is not None:
-        		nums[i], nums[zero_index] = nums[zero_index], nums[i]
-        		zero_index += 1
-        	i += 1
+        for i in xrange(len(nums)):
+        	if nums[i] != 0:
+        		nums[i], nums[zero_idx] = 0, nums[i]
+        		zero_idx += 1
 
-        print nums
+        return nums
 
-print Solution().moveZeroes([0,1])
+print Solution().moveZeroes([0,0,0,0,0,])
