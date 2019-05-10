933ad7ba6c05af2904305539e3febe5ee3e9ee48
sdksfo <sdksfo@gmail.com>
2019-05-09 18:51:52 -0700

    Update medium_445. Add Two Numbers II.py

--- medium_445. Add Two Numbers II.py
+++ medium_445. Add Two Numbers II.py
@@ -1,44 +1,36 @@
-
-class ListNode(object):
-    def __init__(self, x):
-        self.val = x
-        self.next = None
-
-head1 = ListNode(7)
-head1.next = ListNode(2)
-head1.next.next = ListNode(4)
-head1.next.next.next = ListNode(3)
-
-head2 = ListNode(5)
-head2.next = ListNode(6)
-head2.next.next = ListNode(4)
-
 class Solution(object):
-    def get_number(self, head):
-        val = 0
-        while head:
-            val = (val*10) + head.val
-            head = head.next
-        return val
-
-    def get_linkedlist(self, num):
-        if num:
-            prev_node = None
-            while num:
-                new_node = ListNode(num%10)
-                new_node.next = prev_node
-                prev_node = new_node
-                num /= 10
-            return new_node
-        else:
-            return ListNode(0)
+    def reverse(self, l):
+        prev = temp = None
+        while l:
+            temp, l = l, l.next
+            temp.next, prev = prev, temp
+        return temp
 
     def addTwoNumbers(self, l1, l2):
-        total = self.get_number(l1) + self.get_number(l2)
-        return self.get_linkedlist(total)
+        if not l1 and not l2: return None
 
+        l1, l2, carry, num, i = self.reverse(l1), self.reverse(l2), 0, 0, 0
 
-node = Solution().addTwoNumbers(head1, head2)
+        while l1 or l2:
+            total = (l1.val if l1 else 0) + (l2.val if l2 else 0) + carry
+            num += ((total%10)*(10**i))
+            carry = total/10
+            l1, l2 = l1.next if l1 else None, l2.next if l2 else None
+            i += 1
+        else:
+            num += (carry * (10**i))
 
-while node:
-    node = node.next
+        if not num:
+            return ListNode(num)
+
+        head = prev = None
+
+        while num:
+            node = ListNode(num%10)
+            head = head or node
+            num = num/10
+            if prev:
+                prev.next = node
+            prev = node
+
+        return self.reverse(head)
