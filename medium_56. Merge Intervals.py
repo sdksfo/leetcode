68045f7c353a99f0793e674aebaa8cb0654258bf
sdksfo <sdksfo@gmail.com>
2019-05-10 15:57:25 -0700

    Update medium_56. Merge Intervals.py

--- medium_56. Merge Intervals.py
+++ medium_56. Merge Intervals.py
@@ -1,23 +1,18 @@
-# Definition for an interval.
-# class Interval(object):
-#     def __init__(self, s=0, e=0):
-#         self.start = s
-#         self.end = e
+
 
 class Solution(object):
     def merge(self, intervals):
         """
-        :type intervals: List[Interval]
-        :rtype: List[Interval]
+        :type intervals: List[List[int]]
+        :rtype: List[List[int]]
         """
-        intervals = sorted(intervals, key=lambda x: x.start)
-        stack = [intervals[0]] if intervals else []
+        output = []
 
-        for i in xrange(1, len(intervals)):
-            if intervals[i].start <= stack[-1].end:
-                old_interval = stack.pop()
-                new_interval = Interval(old_interval.start, max(old_interval.end, intervals[i].end))
-                stack.append(new_interval)
-            else:
-                stack.append(intervals[i])
-        return stack
+        for interval in sorted(intervals):
+        	start, end = interval
+        	if output and start <= output[-1][1]:
+        		output[-1][1] = max(end, output[-1][1])
+        	else:
+        		output.append([start, end])
+
+     	return output
