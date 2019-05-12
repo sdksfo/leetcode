c0380b8d88b651d1d526aca084b72a79c8cd9eab
santhosh devaraj <santhoshkumar.devaraj@gmail.com>
2019-05-11 19:33:53 -0700

    Update easy_872. Leaf-Similar Trees.py

--- easy_872. Leaf-Similar Trees.py
+++ easy_872. Leaf-Similar Trees.py
@@ -1,12 +1,4 @@
 
-
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
 class Solution(object):
     def leafSimilar(self, root1, root2):
         """
@@ -14,10 +6,9 @@ class Solution(object):
         :type root2: TreeNode
         :rtype: bool
         """
-        def get_sequence(root):
-            if root:
-                if not root.left and not root.right:
-                    return [root.val]
-                return get_sequence(root.left) + get_sequence(root.right)
-            return []
-        return get_sequence(root1) == get_sequence(root2)
+        def getLeaves(root):
+            if not root or (not root.left and not root.right):
+                return [root.val] if root else []
+            return getLeaves(root.left) + getLeaves(root.right)
+
+        return getLeaves(root1) == getLeaves(root2)
