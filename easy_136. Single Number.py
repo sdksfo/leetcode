aef88634e937d5d107c57f595c83867696e9ba26
santhosh devaraj <santhoshkumar.devaraj@gmail.com>
2019-05-11 23:05:12 -0700

    Update easy_136. Single Number.py

--- easy_136. Single Number.py
+++ easy_136. Single Number.py
@@ -1,9 +1,4 @@
-# Explanation
 
-# a) XOR of a number by itself is 0
-# b) XOR of a number by 0 is itself
-
-from functools import reduce
 
 class Solution(object):
     def singleNumber(self, nums):
@@ -11,6 +6,4 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        return reduce((lambda x, y: x ^ y), nums)
-
-print Solution().singleNumber([1,1,2,2,3,4,4])
+        return reduce(lambda x,y: x^y, nums)
